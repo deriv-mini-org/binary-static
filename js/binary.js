8ca49691c84763bdb1afa8e14cc0ac079ab95454
khalidxx <fxtryio@gmail.com>
2021-04-23 16:02:57 +0800

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -23585,7 +23585,7 @@ var TradePage = function () {
     };
 
     var init = function init() {
-        if (Client.isAccountOfType('financial') || Client.isOptionsBlocked()) {
+        if (Client.isAccountOfType('financial')) {
             return;
         }
 
@@ -27970,7 +27970,6 @@ __webpack_require__(/*! jquery.scrollto */ "./node_modules/jquery.scrollto/jquer
 var BinaryLoader = __webpack_require__(/*! ./app/base/binary_loader */ "./src/javascript/app/base/binary_loader.js");
 
 document.addEventListener('DOMContentLoaded', BinaryLoader.init);
-console.log("Loaded...");
 window.onpageshow = function (event) {
     if (event.persisted) {
         window.location.reload();
